692ef606efc59c866a9e79b615e14ace7139b3dd
Julien Eluard <julien.eluard@gmail.com>
2024-01-29 14:13:29 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Exposes an issue when using lodestar with vite.
+
+```sh
+npm install
+npm run dev
+```
+
+Then navigate to the correct URL.
+The following error should be visible in the browser console:
+
+```js
+chunk-EVGRMFEP.js?v=a1c9cd90:7383 Uncaught ReferenceError: process is not defined
+    at chunk-EVGRMFEP.js?v=a1c9cd90:7383:54
+```
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: 'esnext',
+  },
+  esbuild: {
+    target: 'esnext',
+    supported: {
+      'top-level-await': true,
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
+})
